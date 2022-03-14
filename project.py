fdb2fb9b1a7a66ad2b3feecf900912470a452d0c
Aidan J.M. Laird <80402302+AJMLAIRD@users.noreply.github.com>
2022-03-14 11:40:32 +1100

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ import os
 import numpy as np 
 import math
 import time
-import hvplot.pandas
+import hvplot.pandas as hvplot
 from pathlib import Path
 from datetime import date
 from datetime import datetime, timedelta
